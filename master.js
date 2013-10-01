8a98f222b85d49a5ff28bee91ea7d549334deaf4
Daniel X Moore <yahivin@gmail.com>
2013-09-30 17:59:31 -0700

    :confounded::file_folder: Updated in browser at strd6.github.io/editor

--- master.js
+++ master.js
@@ -1,66 +1,64 @@
 (function() {
   var Packager, dependencyScripts, jsonpWrapper, makeScript, packageWrapper, program, reject;
 
-  Packager = function() {
-    return {
-      collectDependencies: function(dependencies) {
-        var names;
-        names = Object.keys(dependencies);
-        return Deferred.when(names.map(function(name) {
-          var branch, callback, match, repo, user, value;
-          value = dependencies[name];
-          if (typeof value === "string") {
-            if (value.startsWith("http")) {
-              return $.getJSON(value);
-            } else {
-              if ((match = value.match(/([^\/]*)\/([^\:]*)\:(.*)/))) {
-                callback = match[0], user = match[1], repo = match[2], branch = match[3];
-                user = user.toLowerCase();
-                return $.ajax({
-                  url: "http://" + user + ".github.io/" + repo + "/" + branch + ".jsonp",
-                  dataType: "jsonp",
-                  jsonpCallback: callback,
-                  cache: true
-                });
-              } else {
-                return reject("Failed to parse repository info string " + value + ", be sure it's in the\nform `<user>/<repo>:<ref>` for example: `STRd6/issues:master`\nor `STRd6/editor:v0.9.1`");
-              }
-            }
+  Packager = {
+    collectDependencies: function(dependencies) {
+      var names;
+      names = Object.keys(dependencies);
+      return Deferred.when(names.map(function(name) {
+        var branch, callback, match, repo, user, value;
+        value = dependencies[name];
+        if (typeof value === "string") {
+          if (value.startsWith("http")) {
+            return $.getJSON(value);
           } else {
-            return reject("Can only handle url string dependencies right now");
+            if ((match = value.match(/([^\/]*)\/([^\:]*)\:(.*)/))) {
+              callback = match[0], user = match[1], repo = match[2], branch = match[3];
+              user = user.toLowerCase();
+              return $.ajax({
+                url: "http://" + user + ".github.io/" + repo + "/" + branch + ".jsonp",
+                dataType: "jsonp",
+                jsonpCallback: callback,
+                cache: true
+              });
+            } else {
+              return reject("Failed to parse repository info string " + value + ", be sure it's in the\nform `<user>/<repo>:<ref>` for example: `STRd6/issues:master`\nor `STRd6/editor:v0.9.1`");
+            }
           }
-        })).then(function(results) {
-          var bundledDependencies;
-          bundledDependencies = {};
-          names.each(function(name, i) {
-            return bundledDependencies[name] = results[i].first();
-          });
-          return bundledDependencies;
+        } else {
+          return reject("Can only handle url string dependencies right now");
+        }
+      })).then(function(results) {
+        var bundledDependencies;
+        bundledDependencies = {};
+        names.each(function(name, i) {
+          return bundledDependencies[name] = results[i].first();
         });
-      },
-      standAlone: function(pkg) {
-        var distribution, entryPoint, html, json, source;
-        source = pkg.source, distribution = pkg.distribution, entryPoint = pkg.entryPoint;
-        html = "<!doctype html>\n<head>\n<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />\n" + (dependencyScripts(pkg.remoteDependencies)) + "\n</head>\n<body>\n<script>\n" + (packageWrapper(pkg, "require('./" + entryPoint + "')")) + "\n<\/script>\n</body>\n</html>";
-        json = JSON.stringify(pkg, null, 2);
-        return {
-          html: html,
-          js: program(pkg),
-          json: json,
-          jsonp: jsonpWrapper(pkg.repository, json)
-        };
-      },
-      testScripts: function(pkg) {
-        var distribution, testProgram;
-        distribution = pkg.distribution;
-        testProgram = Object.keys(distribution).select(function(path) {
-          return path.match(/test\//);
-        }).map(function(testPath) {
-          return "require('./" + testPath + "')";
-        }).join("\n");
-        return "" + (dependencyScripts(pkg.remoteDependencies)) + "\n<script>\n  " + (packageWrapper(pkg, testProgram)) + "\n<\/script>";
-      }
-    };
+        return bundledDependencies;
+      });
+    },
+    standAlone: function(pkg) {
+      var distribution, entryPoint, html, json, source;
+      source = pkg.source, distribution = pkg.distribution, entryPoint = pkg.entryPoint;
+      html = "<!doctype html>\n<head>\n<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />\n" + (dependencyScripts(pkg.remoteDependencies)) + "\n</head>\n<body>\n<script>\n" + (packageWrapper(pkg, "require('./" + entryPoint + "')")) + "\n<\/script>\n</body>\n</html>";
+      json = JSON.stringify(pkg, null, 2);
+      return {
+        html: html,
+        js: program(pkg),
+        json: json,
+        jsonp: jsonpWrapper(pkg.repository, json)
+      };
+    },
+    testScripts: function(pkg) {
+      var distribution, testProgram;
+      distribution = pkg.distribution;
+      testProgram = Object.keys(distribution).select(function(path) {
+        return path.match(/test\//);
+      }).map(function(testPath) {
+        return "require('./" + testPath + "')";
+      }).join("\n");
+      return "" + (dependencyScripts(pkg.remoteDependencies)) + "\n<script>\n  " + (packageWrapper(pkg, testProgram)) + "\n<\/script>";
+    }
   };
 
   module.exports = Packager;
